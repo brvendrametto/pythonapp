cb7beef06d35188b5db096729df5c1e8875e78d8
Bruno <32823607+brvendrametto@users.noreply.github.com>
2023-05-01 16:27:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
 
 @app.route("/services/service-2")
 def hello():
-    return "This is a sample response from service 2 (Python App Service) v2-br"
+    return "Python App Service-br"
 
 
 @app.route("/services/service-2/status")
